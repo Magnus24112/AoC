dc7ec859a42d290ba9ddf6adc835f90c7db9f517
Magnus <56786591+Magnus24112@users.noreply.github.com>
2024-12-22 20:35:01 +0100

    Update day22.py

--- 2024/day22.py
+++ 2024/day22.py
@@ -1,49 +1,19 @@
 from time import perf_counter
-import sys
-sys.setrecursionlimit(10000)
-start = perf_counter()
+import numpy as np
 
 with open('input_day22.txt') as file:
     data = tuple(int(line) for line in file.readlines())
 
-def base10_to_base2(num: int) -> tuple:
-    output = []
-    for i in range(23, -1, -1):
-        if num >= 2**i:
-            num -= 2**i
-            output.append(True)
-        else:
-            output.append(False)
-    return tuple(output)
+
+def evolve_secret_numbers(numbers):
+    for _ in range(2000):
+        numbers = numbers ^ (numbers << 6) & 16777215 # multiply by 64
+        numbers = numbers ^ (numbers >> 5) & 16777215 # divide by 32
+        numbers = numbers ^ (numbers << 11) & 16777215 # multiply by 20248
+    return numbers
 
 
-def base2_to_base10(num: tuple) -> int:
-    return sum(num[-i -1] * 2**i for i in range(24))
-
-
-def evolve_secret_number(num: tuple, n_evolutions: int = 1) -> tuple:
-    if n_evolutions == 0:
-        return num
-
-    evolved_num = mul2048(div32(mul64(num)))
-    return evolve_secret_number(evolved_num, n_evolutions - 1)
-
-
-def bitxor(num1: tuple, num2: tuple) -> tuple:
-    return tuple(item1 != item2 for item1, item2 in zip(num1, num2))
-
-
-def mul64(num: tuple) -> tuple:
-    return bitxor(num, num[6:] + (False, ) * 6)
-
-
-def mul2048(num: tuple) -> tuple:
-    return bitxor(num, num[11:] + (False, ) * 11)
-
-
-def div32(num: tuple) -> tuple:
-    return bitxor(num, (False,) * 5 + num[:-5])
-
-
-print(sum(base2_to_base10(evolve_secret_number(base10_to_base2(number), 2000)) for number in data))
+# part 1
+start = perf_counter()
+print(sum(evolve_secret_numbers(np.array(data))))
 print(perf_counter() - start)
